01aadc7bae6a480634515db9e4882d9db1168326
krit sukrit <sukrit@dek-d.com>
2024-09-24 22:09:37 +0700

    Initial Commit

---
+++ LMCrypto/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LMCrypto
+//
+//  Created by Sukrit Chatmeeboon on 24/9/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LMCrypto/LMCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMCryptoApp.swift
+//  LMCrypto
+//
+//  Created by Sukrit Chatmeeboon on 24/9/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct LMCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
